f4aedea25499308e9c14c9ce8b11d1191b10bd17
Kurt Feigl <feigl@wisc.edu>
2018-05-24 18:10:00 -0500

    more fault

--- faults/triangulatexyz3.m
+++ faults/triangulatexyz3.m
@@ -1,4 +1,4 @@
-function [Kfaces,lgood] = triangulatexyz3(X,Y,Z,titlestr,make_plots)
+function [TRI,lgood,X,Y,Z] = triangulatexyz3(X,Y,Z,titlestr,make_plots)
 %% Generate kfaces of a fault by projecting onto a plane and then triangulating
 %function F = triangulatexyz(XYZ,doplots)
 % inputs:
@@ -15,6 +15,9 @@ function [Kfaces,lgood] = triangulatexyz3(X,Y,Z,titlestr,make_plots)
 %% intialize
 % indices for figures
 nf = 0;
+TRI=struct([]);
+lgood = false;
+
 
 %% check sizes of arrays
 X = colvec(X);nx=numel(X);
@@ -27,28 +30,25 @@ if nx ~= ny || nx ~= nz || ny ~= nz
     nz
     error('input vectors are not the same sizes');
 else
-    npoints = nx;
+    npoints = nx
 end
 
 if npoints < 3
     npoints
     warning('Too few points');
-    Kfaces = NaN;
-    Kboundary = NaN;
     return;
 end
-XYZ = [X,Y,Z];
-% whos XYZ
 
 %% find the best-fitting plane and calculate the modeled values of ZM = f(XM,YM) on a mesh
 %compute the normal to the plane and a point that belongs to the plane
 % normvec : a unit (column) vector normal to the plane
 % basisvecs : a 3 by 2 matrix. Its columns form an orthonormal basis of the plane
 % point0 : a point belonging to the plane
-%
+XYZ = [X,Y,Z];
+% whos XYZ
 [normvec,basisvecs,point0] = affine_fit(XYZ);
 
-%% calculate projections of points into UU,VV plane
+% calculate projections of points into UU,VV plane
 UU=nan(npoints,1);
 VV=nan(npoints,1);
 kount = 0;
@@ -61,23 +61,36 @@ for i=1:npoints
 end
 UU = colvec(UU);
 VV = colvec(VV);
+clear XYZ
+save('UUVV.mat','UU','VV');
+
 
 % remove empty rows
-UU = UU(1:kount);
-VV = VV(1:kount);
+if kount ~= npoints
+    warning('pruning');
+    UU = UU(1:kount);
+    VV = VV(1:kount);
+end
+
+%% remove duplicate rows
+%    [C,IA,IC] = unique(A) also returns index vectors IA and IC such that
+%     C = A(IA) and A = C(IC) (or A(:) = C(IC), if A is a matrix or array).  
+[UUVV,iasort,icsort] = unique([UU,VV],'rows');
+UU=UU(iasort);
+VV=VV(iasort);
+X=X(iasort);
+Y=Y(iasort);
+Z=Z(iasort);
 
 if numel(UU) < 3 || numel(VV) < 3
     UU
     VV
-    Kfaces = nan;
-    Kboundary = nan;
     warning('miscount')
     return
 end
     
-%% try to make our own kfaces
-Kfaces = delaunay(UU,VV);
-[ntriangles,ncols]= size(Kfaces)
+
+
 % theta = nan(3,1);
 
 %% find the boundary in UU,VV plane using fancy routine named alpha shape
@@ -91,18 +104,63 @@ Kfaces = delaunay(UU,VV);
 alpha_radius = 0.1*min([max(colvec(UU))-min(colvec(UU)),max(colvec(VV))-min(colvec(VV))]); % in meters
 shp = alphaShape(UU,VV,alpha_radius);
 Kboundary = boundaryFacets(shp);
-
 % find boundary in U,V plane
 Ubound = [shp.Points(Kboundary(:,1),1) ; shp.Points(Kboundary(:,2),1)];
 Vbound = [shp.Points(Kboundary(:,1),2) ; shp.Points(Kboundary(:,2),2)];
 
-%%  make a mesh
-[XM,YM] = meshgrid(  ...
-     linspace(nanmin(XYZ(:,1)),nanmax(XYZ(:,1)),10)...
-    ,linspace(nanmin(XYZ(:,2)),nanmax(XYZ(:,2)),10));
+if make_plots == 1
+    %% plot points and alpha shape in U-V plane
+    nf=nf+1;figure;hold on;
+    % plot shape, including vertices and edges
+    %plot(shp);
+    %plot vertices only
+    plot(shp.Points(:,1),shp.Points(:,2),'r*');
+    % plot boundary of alpha shape
+    plot(Ubound,Vbound,'b-','LineWidth',3);
+    title(sprintf('%s\n%s',titlestr,'nNonconvex Alpha Shape'));
+end
 
-%%  evalute the formula for the plane
-ZM = -1*(normvec(1)/normvec(3)*XM+normvec(2)/normvec(3)*YM-dot(normvec,point0)/normvec(3));
+
+%%  make a mesh for plotting plane
+if make_plots == 1
+%     stepsize = 10 % in meters
+%     [XM,YM] = meshgrid(  ...
+%          [nanmin(X):stepsize:nanmax(X)]...
+%         ,[nanmin(Y):stepsize:nanmax(Y)]);
+    nsteps = 10; % 
+    [XM,YM] = meshgrid(  ...
+         linspace(nanmin(X),nanmax(X),nsteps)...
+        ,linspace(nanmin(Y),nanmax(Y),nsteps));
+    
+    %%  evalute the formula for the plane
+    ZM = -1*(normvec(1)/normvec(3)*XM+normvec(2)/normvec(3)*YM-dot(normvec,point0)/normvec(3));
+end
+
+
+%% Calculate a triangular mesh using Delaunay triangulation
+% use older routine
+%Kfaces = delaunay(UU,VV);
+% [ntriangles1,ncols1]= size(Kfaces1)
+
+% use newer routine
+TRI = delaunayTriangulation(UU,VV);
+% use boundary of alpha shape as edge constraint
+% TRI = delaunayTriangulation(UU,VV,Kboundary);
+
+if numel(TRI.Points(:,1)) ~= numel(UU)
+    error('miscount')
+    return
+    % Algorithm has added points, update coordinates
+    UU = colvec(TRI.Points(:,1));
+    VV = colvec(TRI.Points(:,2));
+    npoints = numel(UU)
+end
+
+Kfaces = TRI.ConnectivityList;
+% gives different answers
+% https://www.mathworks.com/matlabcentral/answers/252870-why-does-delaunay-sometimes-give-a-different-result-from-delaunaytriangulation
+[ntriangles,ncols]= size(Kfaces)
+
 
 %% evaluate each triangle individually
 lgood = false(ntriangles,1);
@@ -110,7 +168,7 @@ for i=1:ntriangles
     nok = 0;
     k3 = Kfaces(i,[1:3]);
 
-    % test if all three vertices of triangle are inside alpha shape
+%     % test if all three vertices of triangle are inside alpha shape
 %     for j=1:3
 %         k = Kfaces(i,j); % index of vertices in triangle       
 %         if inpolygon(UU(k),VV(k),Ubound,Vbound) == 1
@@ -130,7 +188,7 @@ for i=1:ntriangles
 %     if area > (25.)^2
 %         nok = nok+1;
 %     end
-%    
+   
 %     % We can use the aspect ratio (AR) of the triangle as a rejection
 %     % criterion; the AR being defined as the ratio of the incircle radius to
 %     % circumcircle radius.
@@ -150,85 +208,59 @@ end
 
 %% prune
 igood = find(lgood==true);
-ngood = numel(lgood(igood))
+ngood = numel(igood)
 
 if make_plots == 1
-    %% plot points and alpha shape in U-V plane
+    %% make a 3D plot
     nf=nf+1;figure;
-    plot(shp);
-    hold on;
-    plot(UU,VV,'k.')
-    plot(UU(igood),VV(igood),'ro','MarkerSize',3);
-    %plot(shp.Points(:,1),shp.Points(:,2),'r*');
-    plot(Ubound,Vbound,'b-','LineWidth',3);
-    title(sprintf('%s\n%s',titlestr,'nNonconvex Alpha Shape'));
-
-    %% make a 3D plot    
-    nf=nf+1;figure;
-   %plot3(XYZ(:,1),XYZ(:,2),XYZ(:,3),'r.');
-    plot3(XYZ(igood,1),XYZ(igood,2),XYZ(igood,3),'r.');
+    plot3(X,Y,Z,'r.');
     hold on;
     axis tight;
     axis equal;
-    % plot the mesh
+    % plot the best-fitting plane
     surf(XM,YM,ZM,'facecolor','blue','facealpha',0.5);
     xlabel('X');
     xlabel('Y');
     ylabel('Z');
-    title(sprintf('%s\n%s',titlestr,'3 D'));
-
-%     %% make a 2D plot in UU-VV plane
-%     nf=nf+1;figure;hold on;
-%     % plot points
-%     plot(UU,VV,'b.');
-%     axis xy;axis tight;axis equal;
-%     % plot boundary
-%     %plot(UU(kbound),VV(kbound),'r-','linewidth',5);
-%     plot(Ubound,Vbound,'r-','linewidth',5);
-%     % plot each triangle individually
-%     for j=1:ntriangles
-%         k3 = Kfaces(j,[1,2,3]); % indices of points around triangle
-%         umean = mean(UU(k3));
-%         vmean = mean(VV(k3));
-%         %plot(xmean,ymean,'b*');
-%         %inside = inpolygon(xmean,ymean,UU(kbound),VV(kbound));
-%         inside = inpolygon(umean,vmean,Ubound,Vbound);
-%         if inside == 1
-%             k4 = Kfaces(j,[1,2,3,1]); % indices of points around triangle, closing
-%             plot(colvec(UU(k4)),colvec(VV(k4)),'k-');  % ours
-%         end
-%     end
-%     xlabel('U');
-%     ylabel('V');
-%     title(sprintf('%s\n%s',titlestr,'Projected coordinates with boundary'));
-%     %close all
+    title(sprintf('%s\n%s',titlestr,'3 D')); 
+       
+    %% plot points and alpha shape in U-V plane
+    nf=nf+1;figure;hold on;
+    % plot shape, including vertices and edges
+    %plot(shp);
+    %plot vertices only
+    %plot(shp.Points(:,1),shp.Points(:,2),'r*');
+    plot(UU,VV,'k.')
+    % plot edges as triangles
+    for i=1:numel(igood)
+        k4=colvec(Kfaces(igood(i),[1,2,3,1]));
+        plot(UU(k4),VV(k4),'k.-');
+    end
+    % plot boundary of alpha shape
+    plot(Ubound,Vbound,'b-','LineWidth',3);
+    title(sprintf('%s\n%s',titlestr,'nNonconvex Alpha Shape'));
     
     
-%% plot mesh in 3D
-nf=nf+1;figure;hold on;
-for j=1:numel(igood)
-    %        k3 = colvec(Kfaces(j,[1,2,3]));
-    %         umean = mean(UU(k3));
-    %         vmean = mean(VV(k3));
-    %         %inside = inpolygon(xmean,ymean,UU(kbound),VV(kbound));
-    %         inside = inpolygon(umean,vmean,Ubound,Vbound);
-    %         if inside == 1
-    %             k4=colvec(Kfaces(j,[1,2,3,1]));
-    %             %plot3(colvec(XYZ(k4,1)),colvec(XYZ(k4,2)),colvec(XYZ(k4,3)),'k.-');
-    %             plot3(X(k4),Y(k4),Z(k4),'k.-');
-    %         end
-    k4=colvec(Kfaces(igood(j),[1,2,3,1]));
-    plot3(X(k4),Y(k4),Z(k4),'k.-');
+    %% plot mesh in 3D
+    nf=nf+1;figure;hold on;
+    % plot one triangle at a time
+    for i=1:numel(igood)
+        k4=colvec(Kfaces(igood(i),[1,2,3,1]));
+       %fprintf(1,'%5d %5d %5d %5d %5d\n',i,k4);
+        plot3(X(k4),Y(k4),Z(k4),'k.-');
+    end
+    axis tight;
+    axis equal;
+    title(sprintf('%s\n%s',titlestr,'Mesh'),'Interpreter','none');
+    xlabel('X');
+    ylabel('Y');
+    zlabel('Z');
+    grid on;
+    view([1, -1, 1]);
+    %printpng(sprintf('%s_%s.png',mfilename,strrep(titlestr,'/','_')));
+    close all    
 end
-axis tight;
-axis equal;
-title(sprintf('%s\n%s',titlestr,'Mesh'),'Interpreter','none');
-xlabel('X');
-ylabel('Y');
-zlabel('Z');
-grid on;
-view([1, -1, 1]);
-%printpng(sprintf('%s_%s.png',mfilename,strrep(titlestr,'/','_')));
+
 
 
 return
